9fb892db52a9bb0d3253ca641fb0f2544bf628ec
sophie.chhoeu <sophie.chhoeu@carsguide.com.au>
2018-04-17 23:57:39 +1000

    updated frontend react with backend proxy

--- client/src/App.js
+++ client/src/App.js
@@ -9,10 +9,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hi there</h1>
+
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign in with google</a>
       </div>
     );
   }
